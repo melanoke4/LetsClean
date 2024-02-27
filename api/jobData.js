c1fd124d3288d6e19532cb573b56be6dd077d075
Melanie Warwick <princessmelanoke@gmail.com>
2024-02-26 19:38:53 -0600

    I promised jobs on the dom

---
+++ api/jobData.js
@@ -0,0 +1,73 @@
+import { clientCredentials } from '../utils/client';
+
+const endpoint = clientCredentials.databaseURL;
+
+const getJobs = (uid) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/jobs.json?orderBy="uid"&equalTo="${uid}"`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
+const deleteJob = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/jobs/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve((data)))
+    .catch(reject);
+});
+
+const getSingleJob = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/jobs/${firebaseKey}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const createJob = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/jobs.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const updateJob = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/jobs/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+export {
+  getJobs,
+  deleteJob,
+  getSingleJob,
+  updateJob,
+  createJob,
+};
